05f3d2d70433471089249b9b6325f81e76afeb8b
Jay Engineer <jayengineer6@gmail.com>
2014-07-16 00:55:33 -0400

    restructuring code

--- src/__init__.py
+++ src/__init__.py
@@ -4,39 +4,49 @@ import time
 import requests
 import urllib2
 
-def getRandomData():
+def check_request(url):
+	try:
+	    return requests.get(url).status_code
+	except Exception as e:
+  	    print str(e)
+
+def generate_data():
 	url = "http://numbersapi.com/random?json"
 	results = json.load(urllib2.urlopen(url))
-	#data = str(results["text"].replace(".","?"))
 	data = str(results["text"]) + "#BellLetsTalk"
 
-	if len(data) < 140:
-		return data
-	else:
-		getRandomData()
+	return data
 
-def main():
+def main(**kwargs):
+	url = "http://numbersapi.com/random?json"
+	consumer_key = kwargs.get("consumer_key")
+	consumer_secret = kwargs.get("consumer_secret")
+	access_token_key = kwargs.get("access_token_key")
+	access_token_secret = kwargs.get("access_token_secret")
+	api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-	api = twitter.Api(consumer_key='Q2EGXTPpwqeIfrvQiHUAQ',
-	                  consumer_secret='2QkSVjD7lW3fsTsX0wU3Q8jrRsYKFLCkZBUZv77ZuCA',
-	                  access_token_key='606652366-3a0ieR32ysCusf5ANdU0ukwF8ORCVcz0lsYjCilz',
-	                  access_token_secret='icI8PQK0dt36Kzi1USyASOzeO18YuPEMGdYWjwnXooJJu')
+	results = generate_data()
 
-	#print api.VerifyCredentials()
-
-	results = getRandomData()
-	status = api.PostUpdate(results)
-
-if __name__ == '__main__':
 	count = 1
-	while True:
-		main()
 
+	while True:
 		print count
-		#still can be improved
+
+		if check_request(url) is not 200:
+			raise Exception("bad request")
+		elif check_request(url) == 200:
+			try:
+				count +=1
+				status = api.PostUpdate(results)
+				if len(results) < 140:
+					continue
+			except Exception as e:
+				print str(e)
+
 		time.sleep(2)
 
 		if count > 10:
 			break
 
-		count +=1
+if __name__ == '__main__':
+	print main(consumer_key="Q2EGXTPpwqeIfrvQiHUAQ", consumer_secret='2QkSVjD7lW3fsTsX0wU3Q8jrRsYKFLCkZBUZv77ZuCA', access_token_key='606652366-3a0ieR32ysCusf5ANdU0ukwF8ORCVcz0lsYjCilz', access_token_secret='icI8PQK0dt36Kzi1USyASOzeO18YuPEMGdYWjwnXooJJu')
