f2b1f885b1a2fb6a47cde7ae26b32e562c803907
Jay Engineer <jayengineer6@gmail.com>
2014-07-16 01:00:58 -0400

    w

--- src/__init__.py
+++ src/__init__.py
@@ -5,32 +5,32 @@ import requests
 import urllib2
 
 def check_request(url):
-	try:
-		return requests.get(url).status_code
-	except Exception as e:
-		print str(e)
+    try:
+        return requests.get(url).status_code
+    except Exception as e:
+        print str(e)
 
 def generate_data():
-	url = "http://numbersapi.com/random?json"
-	results = json.load(urllib2.urlopen(url))
-	data = str(results["text"]) + "#BellLetsTalk"
+    url = "http://numbersapi.com/random?json"
+    results = json.load(urllib2.urlopen(url))
+    data = str(results["text"]) + "#BellLetsTalk"
 
-	return data
+    return data
 
 def main(**kwargs):
-	url = "http://numbersapi.com/random?json"
-	consumer_key = kwargs.get("consumer_key")
-	consumer_secret = kwargs.get("consumer_secret")
-	access_token_key = kwargs.get("access_token_key")
-	access_token_secret = kwargs.get("access_token_secret")
-	api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
-	results = generate_data()
-	count = 1
+    url = "http://numbersapi.com/random?json"
+    consumer_key = kwargs.get("consumer_key")
+    consumer_secret = kwargs.get("consumer_secret")
+    access_token_key = kwargs.get("access_token_key")
+    access_token_secret = kwargs.get("access_token_secret")
+    api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
+    results = generate_data()
+    count = 1
 
-	while True:
-        print count
+    while True:
+    	print count
 
-      	if check_request(url) is not 200:
+    	if check_request(url) is not 200:
       	    raise Exception("bad request")
       	elif check_request(url) == 200:
             try:
